a05a8165de9742945c6adf9068440378aec0be25
Peralta <peraltar91@my.sunyulster.edu>
2019-04-09 11:23:55 -0400

    Finished prototype
    done, upload it now.

--- Assets/ApplePicker.cs
+++ Assets/ApplePicker.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class ApplePicker : MonoBehaviour {
 	[Header("Set in Inspector")]
@@ -8,14 +10,40 @@ public class ApplePicker : MonoBehaviour {
 	public int numBaskets = 3;
 	public float basketBottomY = -14f;
 	public float basketSpacingY = 2f;
+	public List<GameObject> basketList;
+
 
 	void Start () {
+		basketList = new List<GameObject> ();
+
 		for (int i = 0; i < numBaskets; i++) 
 		{
-			GameObject tBasketGO = Instantiate<GameObject> (basketPrefab);
+			GameObject tBasketGO = Instantiate<GameObject>
+				(basketPrefab);	
 			Vector3 pos = Vector3.zero;
 			pos.y = basketBottomY + (basketSpacingY * i);
 			tBasketGO.transform.position = pos;
+			basketList.Add (tBasketGO);
+		}
+
+	}
+
+	public void AppleDestroyed(){
+		// get a list of all the apples on the screen
+		GameObject[] tAppleArray = GameObject.FindGameObjectsWithTag("Apple");
+		foreach (GameObject tGO in tAppleArray) {
+			Destroy (tGO);
+		}
+
+		// We dropped an apple so get rid of a basket
+		int basketIndex = basketList.Count-1; // get index of last basket
+		GameObject tBasketGO = basketList[basketIndex];
+		basketList.RemoveAt (basketIndex);
+		Destroy (tBasketGO);
+
+		// if we ran out baskets we lose!
+		if (basketList.Count == 0) {
+			SceneManager.LoadScene ("_Scene_0");
 		}
 
 	}
